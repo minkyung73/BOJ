38f48dde0d6802412bf6b408dfa0862cd82dbad2
minkyung <minkyung7365@gmail.com>
2023-06-19 15:33:31 +0900

    java 기초

---
+++ Java/boj_11720.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class boj_11720 {
+    public static void main(String[] args){
+//        숫자의 합
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String numbers = sc.next();
+
+        int sum = 0;
+
+        for(int i=0 ; i<n ; i++){
+            int temp = Character.getNumericValue(numbers.charAt(i));
+
+            if(temp == 0)
+                continue;
+            else
+                sum += temp;
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ Java/boj_2675.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class boj_2675 {
+    public static void main(String[] args){
+//        문자열 반복
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int[] iter = new int[num];
+        String[] strings = new String[num];
+
+        for(int i=0 ; i<num ; i++){
+            iter[i] = sc.nextInt();
+            strings[i] = sc.next();
+        }
+
+        for(int i=0 ; i<num ; i++){
+            char[] chars = strings[i].toCharArray();
+
+            for(char ch : chars){
+                for(int j=0 ; j<iter[i] ; j++)
+                    System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
