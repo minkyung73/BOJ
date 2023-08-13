ef246a9faf052fddbdf5176a43e40dc2976d4914
minkyung <minkyung7365@gmail.com>
2023-08-13 17:14:38 +0900

    [ADD]2579

---
+++ Java/boj_2579.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class boj_2579 {
+    public static void main(String[] args) {
+        // 계단 오르기
+
+        // input
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<Integer> stairs = new ArrayList<>();
+        for(int i=0 ; i<N ; i++)
+            stairs.add(sc.nextInt());
+
+
+    }
+}
