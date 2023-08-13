c1eb8cbc088a283c2cb1b7d97d7ed65bf0314c9f
minkyung <minkyung7365@gmail.com>
2023-08-13 18:10:35 +0900

    [ADD]2579

--- Java/boj_2579.java
+++ Java/boj_2579.java
@@ -14,6 +14,17 @@ public class boj_2579 {
         for(int i=0 ; i<N ; i++)
             stairs.add(sc.nextInt());
 
+        List<Integer> score = new ArrayList<>();
+        score.add(stairs.get(0));
+        if(N > 1)
+            score.add(stairs.get(0) + stairs.get(1));
+        if(N > 2)
+            score.add(Math.max(stairs.get(0), stairs.get(1)) + stairs.get(2));
 
+        for(int i = 3 ; i< N ; i++)
+            score.add(Math.max(score.get(i-3)+stairs.get(i-1), score.get(i-2)) + stairs.get(i));
+
+        // output
+        System.out.println(score.get(N-1));
     }
 }
