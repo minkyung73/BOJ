043f1bc0f3673e5a301bf99db255836f702f9820
minkyung <minkyung7365@gmail.com>
2023-10-16 14:25:05 +0900

    1931

---
+++ Java/boj_1931.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class boj_1931 {
+    // 회의실 배정
+    private static int n;
+    private static int[][] arr;
+//    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][2];
+
+        for(int i=0 ; i<n ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0];
+            }
+        });
+
+        int cnt = 0;
+        int time = 0;
+        for(int i=0 ; i<n ; i++) {
+            if(arr[i][0] >= time) {
+                time = arr[i][1];
+                cnt++;
+            }
+        }
+
+        // output
+        System.out.println(cnt);
+    }
+}
