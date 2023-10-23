574e07acaa5d2f38b10188f42c1afbfd13e1b610
minkyung <minkyung7365@gmail.com>
2023-10-23 14:27:41 +0900

    swea_2019-더블더블

---
+++ Java/swea/swea_2019.java
@@ -0,0 +1,22 @@
+package swea;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class swea_2019 {
+    // 더블더블
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+
+        int num = 1;
+        for(int i=0 ; i<=n ; i++) {
+            sb.append(num + " ");
+            num *= 2;
+        }
+        System.out.println(sb);
+    }
+}
