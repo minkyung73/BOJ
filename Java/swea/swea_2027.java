50786992ab343c955f78e0cf745d461e41b4efa3
minkyung <minkyung7365@gmail.com>
2023-10-23 14:13:45 +0900

    swea_2027-대각선 출력하기

---
+++ Java/swea/swea_2027.java
@@ -0,0 +1,15 @@
+package swea;
+
+import java.io.IOException;
+
+public class swea_2027 {
+    // 대각선 출력하기
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("#++++");
+        System.out.println("+#+++");
+        System.out.println("++#++");
+        System.out.println("+++#+");
+        System.out.println("++++#");
+    }
+}
