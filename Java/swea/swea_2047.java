5ce0801b8557bad023ea4f91de301ae00035f8dc
minkyung <minkyung7365@gmail.com>
2023-10-23 14:03:32 +0900

    swea_2047-신문 헤드라인

---
+++ Java/swea/swea_2047.java
@@ -0,0 +1,16 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class swea_2047 {
+    // 신문 헤드라인
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        System.out.println(str.toUpperCase());
+    }
+}
