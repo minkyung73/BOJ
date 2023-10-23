1c19122fe037583fab813b323bc038690f1e6161
minkyung <minkyung7365@gmail.com>
2023-10-23 14:08:48 +0900

    swea_2043-서랍의 비밀번호

---
+++ Java/swea/swea_2043.java
@@ -0,0 +1,20 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_2043 {
+    // 서랍의 비밀번호
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int p = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(p - k + 1);
+    }
+}
