c7ec068b109ed7c127d03962ef3b8cafda30224d
minkyung <minkyung7365@gmail.com>
2023-10-23 14:17:41 +0900

    swea_1938-아주 간단한 계산기

---
+++ Java/swea/swea_1938.java
@@ -0,0 +1,23 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_1938 {
+    // 아주 간단한 계산기
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
