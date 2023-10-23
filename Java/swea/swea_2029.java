1a807450d7cd6a0d890715a5f109a6b5ec8f0fbc
minkyung <minkyung7365@gmail.com>
2023-10-23 14:12:02 +0900

    몫과 나머지 출력하기

---
+++ Java/swea/swea_2029.java
@@ -0,0 +1,27 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_2029 {
+    // 몫과 나머지 출력하기
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0 ; i<T ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("#" + (i+1) + " " + a/b + " " + a%b + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
