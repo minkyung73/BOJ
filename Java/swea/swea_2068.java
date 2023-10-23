d1bdb4ccaf32bf1ea34e6a338236b686837d7675
minkyung <minkyung7365@gmail.com>
2023-10-23 13:35:14 +0900

    swea_20680-최대수 구하기

---
+++ Java/swea/swea_2068.java
@@ -0,0 +1,30 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_2068 {
+    // 최대수 구하기
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0 ; i<T ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int max = -1;
+            for(int j=0 ; j<10 ; j++) {
+                int temp = Integer.parseInt(st.nextToken());
+                if(temp > max) max = temp;
+            }
+
+            sb.append("#" + (i+1) + " " + max + "\n");
+        }
+        System.out.println(sb);
+    }
+}
