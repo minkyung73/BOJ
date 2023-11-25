ae4a2e6778e7d3b9aeb2069ef2c06b6f54ee1abd
minkyung <minkyung7365@gmail.com>
2023-11-25 12:08:50 +0900

    boj_1991

---
+++ Java/boj/silver/_1/boj_1991.java
@@ -0,0 +1,78 @@
+package boj.silver._1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1991 {
+    // 트리 순회
+    private static Node head = new Node('A', null, null);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        // insert
+        for(int i=0 ; i<n ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            char root = st.nextToken().charAt(0);
+            char left = st.nextToken().charAt(0);
+            char right = st.nextToken().charAt(0);
+
+            insertNode(head, root, left, right);
+        }
+
+        preOrder(head);
+        System.out.println();
+        inOrder(head);
+        System.out.println();
+        postOrder(head);
+    }
+
+    public static void insertNode(Node temp, char root, char left, char right) {
+        if(temp.value == root) {
+            temp.left = (left == '.') ? null : new Node(left, null, null);
+            temp.right = (right == '.') ? null : new Node(right, null, null);
+        }
+        else {
+            if(temp.left != null) insertNode(temp.left, root, left, right);
+            if(temp.right != null) insertNode(temp.right, root, left, right);
+        }
+    }
+
+    public static void preOrder(Node node) {
+        if(node == null) return;
+        System.out.print(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public static void inOrder(Node node) {
+        if(node == null) return;
+        inOrder(node.left);
+        System.out.print(node.value);
+        inOrder(node.right);
+    }
+
+    public static void postOrder(Node node) {
+        if(node == null) return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value);
+    }
+
+    public static class Node {
+        char value;
+        Node left;
+        Node right;
+
+        public Node(char value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
