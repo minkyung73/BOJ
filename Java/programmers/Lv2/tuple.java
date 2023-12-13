fff9c0d479b59b46b62d07a92bef25d3a752aafb
minkyung <minkyung7365@gmail.com>
2023-12-13 14:20:56 +0900

    [RE]tuple

--- Java/programmers/Lv2/tuple.java
+++ Java/programmers/Lv2/tuple.java
@@ -1,36 +1,55 @@
 package programmers.Lv2;
 
+import java.io.IOException;
 import java.util.*;
 
 class tuple {
-    public int[] solution(String s) {
+    public static void main(String[] args) throws IOException {
+        String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+        System.out.println(Arrays.toString(solution(s)));
+    }
+
+    public static int[] solution(String s) {
         int[] answer;
-        List<Integer> result = new ArrayList<>();
 
-        // n-tuple 만들기
-        String[] tuple_str = s.split("},");
-        answer = new int[tuple_str.length];
+        // step 1. "},"로 split
+        String[] split1 = s.split("},");
 
-        for(int i=0 ; i<tuple_str.length ; i++) {
-            tuple_str[i] = tuple_str[i].replace("{", "");
-            tuple_str[i] = tuple_str[i].replace("}", "");
+        // step 2. "{"과 "}" 제거
+        for(int i=0 ; i<split1.length ; i++) {
+            split1[i] = split1[i].replace("{", "");
+            split1[i] = split1[i].replace("}", "");
         }
 
-        // 길이 순서대로 정렬
-        Arrays.sort(tuple_str, (o1, o2) -> o1.length() < o2.length() ? -1 : 1);
+        // step 3. int 형변환
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0 ; i<split1.length ; i++) {
+            String[] split2 = split1[i].split(",");
 
-        // 원소 구하기
-        for(int i=0 ; i<tuple_str.length ; i++) {
-            String[] temp = tuple_str[i].split(",");
+            list.add(new ArrayList<>());
+            for(int j=0 ; j < split2.length ; j++) {
+                list.get(i).add(Integer.parseInt(split2[j]));
+            }
+        }
 
-            for(int j=0 ; j<temp.length ; j++) {
-                int num = Integer.parseInt(temp[j]);
-                if(!result.contains(num)) {
-                    result.add(num);
+        // step 4. 원소 개수에 따라 오름차순 정렬
+        Collections.sort(list, (o1, o2) -> o1.size() < o2.size() ? -1 : 1);
+
+        // step 5. result 구하기
+        List<Integer> result = new ArrayList<>();
+        for(int i=0 ; i<list.size() ; i++) {
+            List<Integer> temp = list.get(i);
+
+            for(int j=0 ; j<temp.size() ; j++) {
+                if(!result.contains(temp.get(j))) {
+                    result.add(temp.get(j));
+                    break;
                 }
             }
         }
 
+        // step 6. result 값을 answer에 옮기기
+        answer = new int[result.size()];
         for(int i=0 ; i<result.size() ; i++)
             answer[i] = result.get(i);
 
