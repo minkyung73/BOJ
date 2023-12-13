c3c5325c8c287033e9cba6ffb35db532a1178ef1
minkyung <minkyung7365@gmail.com>
2023-12-13 13:55:21 +0900

    [RE]jump_and_tepleport

--- Java/programmers/Lv2/jump_and_teleport.java
+++ Java/programmers/Lv2/jump_and_teleport.java
@@ -1,16 +1,22 @@
 package programmers.Lv2;
 
+import java.io.IOException;
 import java.util.*;
 
 public class jump_and_teleport {
-    public int solution(int n) {
+    public static void main(String[] args) throws IOException {
+        int n = 5;
+
+        System.out.println(solution(n));
+    }
+    public static int solution(int n) {
         int ans = 0;
 
-        while(n > 0) {
-            if(n % 2 == 1)
+        while (n != 0) {
+            if(n % 2 != 0)
                 ans++;
 
-            n /= 2;
+            n/=2;
         }
 
         return ans;
