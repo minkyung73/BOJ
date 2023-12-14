fc69f044ecc314b6d59c7e0d742f83ec562c7f18
minkyung <minkyung7365@gmail.com>
2023-12-14 14:55:33 +0900

    [RE]claw_machine_game

--- Java/programmers/Lv1/claw_machine_game.java
+++ Java/programmers/Lv1/claw_machine_game.java
@@ -3,44 +3,52 @@ package programmers.Lv1;
 import java.util.*;
 
 class claw_machine_game {
-    int answer = 0;
-    List<Integer> basket = new ArrayList<>();
+    public static void main(String[] args) {
+        int[][] board = {
+                {0,0,0,0,0},
+                {0,0,1,0,3},
+                {0,2,5,0,1},
+                {4,2,4,4,2},
+                {3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
 
-    public int solution(int[][] board, int[] moves) {
-        int size = board.length;
+        System.out.println(solution(board, moves));
+    }
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        int board_size = board.length;
+        List<Integer> basket = new ArrayList<>();
 
-        for(int i=0 ; i<moves.length ; i++){
-            pick(board, moves[i]-1, size);
-            ppang();
+        for(int i=0 ; i<moves.length ; i++) {
+            int move = moves[i]-1;
+            int doll = 0;
+
+            // 인형 뽑기
+            for(int j=0 ; j < board_size ; j++) {
+                if(board[j][move] != 0) {
+                    doll = board[j][move];
+                    board[j][move] = 0;
+                    break;
+                }
+            }
+
+            if(doll!=0) basket.add(doll);
+
+            // 같은 인형 터뜨리기
+            boolean flag = true;
+            while (basket.size() >= 2 && flag) {
+                int size = basket.size();
+                if(basket.get(size-1).equals(basket.get(size-2))) {
+                    basket.remove(size-1);
+                    basket.remove(size-2);
+                    answer += 2;
+                    continue;
+                }
+
+                flag = false;
+            }
         }
 
         return answer;
-    }
-
-    public void pick(int[][] board, int move, int size) {
-        // 인형 뽑기
-        int num = 0;
-
-        for(int i=0 ; i < size ; i++) {
-            if(board[i][move] != 0) {
-                num = board[i][move];
-                board[i][move] = 0;
-                basket.add(num);
-                break;
-            }
-        }
-    }
-
-    public void ppang() {
-        if(basket.size() < 2) return ;
-
-        // 제일 끝에 있는 인덱스
-        int idx = basket.size() - 1;
-
-        if(basket.get(idx) == basket.get(idx-1)) {
-            basket.remove(idx);
-            basket.remove(idx-1);
-            answer += 2;
-        }
     }
 }
