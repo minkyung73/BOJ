32dbd3ad097d1d3c0b6bb11a77cd1c6827e41937
minkyung <minkyung7365@gmail.com>
2023-12-15 12:28:28 +0900

    boj_1916

---
+++ Java/boj/gold/_5/boj_1916.java
@@ -0,0 +1,83 @@
+package boj.gold._5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj_1916 {
+    // 최소비용 구하기
+    private static int n, m;
+    private static List<List<Node>> list = new ArrayList<>();
+    private static int[] dist;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        dist = new int[n+1];
+        visited = new boolean[n+1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        for(int i=0 ; i<=n ; i++) list.add(new ArrayList<>());
+
+        for(int i=0 ; i<m ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            list.get(start).add(new Node(end, weight));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int startNum = Integer.parseInt(st.nextToken());
+        int endNum = Integer.parseInt(st.nextToken());
+
+        System.out.println(dijkstra(startNum, endNum));
+    }
+
+    public static int dijkstra(int start, int end) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[n+1];
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+            int cur = curNode.end;
+
+            if (!visited[cur]) {
+                visited[cur] = true;
+
+                for (Node node : list.get(cur)) {
+                    if(!visited[node.end] && dist[node.end] > dist[cur] + node.weight) {
+                        dist[node.end] = dist[cur] + node.weight;
+                        pq.add(new Node(node.end, dist[node.end]));
+                    }
+                }
+            }
+        }
+
+        return dist[end];
+    }
+}
+
+class Node implements Comparable<Node> {
+    int end;
+    int weight;
+
+    public Node(int end, int weight) {
+        this.end = end;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return weight - o.weight;
+    }
+}
---
+++ Java/programmers/Lv2/delivery.java
@@ -0,0 +1,25 @@
+package programmers.Lv2;
+
+public class delivery {
+    // 배달
+    public static void main(String[] args) {
+        int N = 5;
+        int[][] road = {
+                {1,2,1},
+                {2,3,3},
+                {5,2,2},
+                {1,4,2},
+                {5,3,1},
+                {5,4,2}
+        };
+        int K = 3;
+
+        System.out.println(solution(N, road, K));
+    }
+
+    public static int solution(int N, int[][] road, int K) {
+        int answer = 0;
+
+        return answer;
+    }
+}
