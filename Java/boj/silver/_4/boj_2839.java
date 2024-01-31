ce814b7f716f33bd6e1c455206962ba375b8b639
minkyung7365@gmail.com <minkyung7365@gmail.com>
2024-01-31 15:06:26 +0900

    [MOD]boj_2839설탕배달

--- Java/boj/silver/_4/boj_2839.java
+++ Java/boj/silver/_4/boj_2839.java
@@ -1,32 +1,80 @@
 package boj.silver._4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class boj_2839 {
+	static int N;
+	static int ans = Integer.MAX_VALUE;
+	
     public static void main(String[] args) {
         // 설탕배달
 
         // input
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        N = sc.nextInt();
+        
+        // 예외 테케가 있어서 실패
+//        greedy(N);
+        
+        // 시간초과
+//        recursive(N, 0);
+//        System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+        
+        //
+        dp();
+    }
+    
+    private static void dp() {
+		/**
+		 * 1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
+		 * x  x  1  x  1  2  x  2  3  2   3   4   3   4   3
+		 * -> 규칙
+		 * 		dp[i] = dp[i-3] + 1 이거나 
+		 * 		dp[i] = dp[i-5] + 1
+		 * 
+		 * -> 즉, 
+		 * 		Math.min(dp[i-3], dp[i-5]) + 1
+		 */
+    	
+    	// initialize
+    	int[] dp = new int[N+1];
+    	Arrays.fill(dp, 2000);
+    	dp[3] = 1;
+    	if(N >= 5) dp[5] = 1;
+    	
+    	for(int i=6;  i<dp.length ; i++) {
+    		dp[i] = Math.min(dp[i-3], dp[i-5]) + 1;
+    	}
+    	System.out.println(dp[N] >= 2000 ? -1 : dp[N]);
+	}
 
-        // init
-        int min_total_num = -1;
-        int num3 = 0;
+	private static void recursive(int n, int cnt) {
+		// basis part
+    	if(n < 0) {
+    		return ;
+    	}
+    	if(n == 0) {
+    		ans = Math.min(ans,  cnt);
+    		return ;
+    	}
+    	
+    	// inductive part
+    	recursive(n-5, cnt+1);
+    	recursive(n-3, cnt+1);
+	}
 
-        // find the min_total_num
-        while(3 * num3 <= N) {
-            if((N - (3 * num3)) % 5 == 0){
-                int num5 = (N - (3 * num3))/5;
-                int temp = num3 + num5;
-                if(min_total_num == -1) min_total_num = temp;
-                else min_total_num = (min_total_num < temp) ? min_total_num : temp;
-            }
-
-            num3++;
-        }
-
-        // output
-        System.out.println(min_total_num);
+	private static void greedy(int n) {
+    	int cnt = 0;
+    	while(n % 5 != 0) {
+    		n -= 3;
+    		cnt++;
+    	}
+    	
+    	if(n < 0) System.out.println(-1);
+    	else {
+    		cnt += n/5;
+    		System.out.println(cnt);
+    	}
     }
 }
