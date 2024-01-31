87cad091f6a33691b56a0e9482af42619c45e75a
minkyung7365@gmail.com <minkyung7365@gmail.com>
2024-01-31 17:45:43 +0900

    boj_15650 N과 M (2)

---
+++ Java/boj/silver/_3/boj_15650.java
@@ -0,0 +1,54 @@
+package boj.silver._3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_15650 {
+	// N과 M (2) - 수열
+	static int n, m;
+	static int[] selected;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		// initialize
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		selected = new int[m];
+		visited = new boolean[n+1];
+		
+		// function call
+		recursive(0, 0);
+		
+		// output
+		System.out.println(sb);
+	}
+
+	private static void recursive(int k, int idx) {
+		// basis part
+		if(k == m) {
+			for (int i : selected)
+				sb.append(i).append(" ");
+			sb.append("\n");
+			return ;
+		}
+		
+		// inductive part
+		for(int i=idx+1 ; i<=n ; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				selected[k] = i;
+				recursive(k+1, i);
+				visited[i] = false;
+			}
+		}
+	}
+	
+	
+}
