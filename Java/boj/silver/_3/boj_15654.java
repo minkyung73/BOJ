997d782c4548725734d38e68c41ad5fb983c2103
minkyung7365@gmail.com <minkyung7365@gmail.com>
2024-02-01 17:43:19 +0900

    boj_15654 N과 M(5)

---
+++ Java/boj/silver/_3/boj_15654.java
@@ -0,0 +1,59 @@
+package boj.silver._3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_15654 {
+	// N과 M (5) - 순열
+	static int n, m;
+	static int[] arr;
+	static int[] selected;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		// initialize
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		selected = new int[m];
+		visited = new boolean[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0 ; i<n ; i++) arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+
+		// function call
+		recursive(0, 0);
+
+		// output
+		System.out.println(sb);
+	}
+
+	private static void recursive(int k, int idx) {
+		// basis part
+		if (k == m) {
+			for (int i : selected)
+				sb.append(i).append(" ");
+			sb.append("\n");
+			return;
+		}
+
+		// inductive part
+		for (int i = 0; i < n; i++) {
+			if(!visited[i]) {				
+				visited[i] = true;
+				selected[k] = arr[i];
+				recursive(k + 1, i);
+				visited[i] = false;
+			}
+		}
+	}
+
+}
