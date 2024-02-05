0c1c5188c53437877f4cfe6073f7e1b6bc848310
minkyung7365@gmail.com <minkyung7365@gmail.com>
2024-02-05 16:59:33 +0900

    boj_16234

--- Java/boj/gold/_4/boj_16234.java
+++ Java/boj/gold/_4/boj_16234.java
@@ -10,129 +10,137 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj_16234 {
-	// 인구 이동
-
+	// 인구이동
+	
 	static int n, l, r;
-	static int day;
 	static int[][] map;
-//	static boolean[][] visited;
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
+	static boolean[][] visited;
+	static int[] dx = {1, 0, 0, -1};
+	static int[] dy = {0, 1, -1, 0};
+	static int day;
 	static List<List<Node>> list;
 	static List<Integer> average;
-
-	public static void main(String[] args) throws IOException {
+	
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		// initialize
-		day = 0;
+		
+		// n, l, r input
 		n = Integer.parseInt(st.nextToken());
 		l = Integer.parseInt(st.nextToken());
 		r = Integer.parseInt(st.nextToken());
+		
+		// initialize
 		map = new int[n][n];
+		day = 0;
 
 		// map input
-		for (int i = 0; i < n; i++) {
+		for(int i=0 ; i<n ; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
+			for(int j=0 ; j<n ; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
+		
 		// 인구 이동
-		boolean flag = true;
-		while (flag) {
-			flag = false;
+		while(true) {
+			// initialize
+			visited = new boolean[n][n];
 			list = new ArrayList<>();
 			average = new ArrayList<>();
-
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					for (int k = 0; k < 4; k++) {
-						int nx = i + dx[k];
-						int ny = j + dy[k];
-
-						if (checkRange(nx, ny) 
-								&& checkPopulation(new Node(i, j), new Node(nx, ny)) ) {
-							System.out.println("(" + i + "," + j + ")"); 
-							BFS(i, j);
-							flag = true;
-							break;
-						}
+			
+			// BFS
+			for(int i=0 ; i<n ; i++) {
+				for(int j=0 ; j<n ; j++) {
+					if(!visited[i][j] && checkPopulation(new Node(i, j))) {
+						list.add(new ArrayList<>());
+						BFS(i, j);
 					}
 				}
 			}
-
-			if (flag) day++;
 			
-			// update population
-			for (int i = 0; i < average.size(); i++) {
-				for (int j = 0; j < list.size(); j++) {
+			// list 의 사이즈가 0이면 더이상 업데이트 할 게 없으므로 break
+			// 아니라면 업데이트 해야하므로 day++
+			if(list.size() == 0) break;
+			else day++;
+			
+			// 값을 업데이트
+			for(int i=0 ; i<list.size() ; i++) {
+				int avg = average.get(i);
+				
+				for(int j=0 ; j<list.get(i).size() ; j++) {
 					int nx = list.get(i).get(j).x;
 					int ny = list.get(i).get(j).y;
-					map[nx][ny] = average.get(i);
+					
+					map[nx][ny] = avg;
 				}
 			}
 			
-			// 찍어보자
-			System.out.println("day: " + day);
-			for(int i=0 ; i<n ; i++) {
-				for(int j=0 ; j<n ;j++) {
-					System.out.print(map[i][j] + " ");
-				}
-				System.out.println();
-			}
-			System.out.println("===========================");
-		}
-
+		} // end of while
+		
+		// output
 		System.out.println(day);
 	}
-
+	
 	public static void BFS(int x, int y) {
 		Queue<Node> queue = new LinkedList<>();
-		boolean[][] v = new boolean[n][n];
-
 		queue.offer(new Node(x, y));
-		v[x][y] = true;
-		list.add(new ArrayList<>());
-
-		int sum = map[x][y];
-		int cnt = 1;
-
-		while (!queue.isEmpty()) {
-			Node now = queue.poll();
-			list.get(list.size() - 1).add(now);
-
-			for (int i = 0; i < 4; i++) {
+		visited[x][y] = true;
+		
+		int sum = 0;
+		int cnt = 0;
+		
+		while(!queue.isEmpty()) {
+			Node now = queue.poll();	
+			
+			sum += map[now.x][now.y];
+			cnt++;
+			list.get(list.size()-1).add(new Node(now.x, now.y));
+			
+			for(int i=0 ; i<4 ; i++) {
 				int nx = now.x + dx[i];
 				int ny = now.y + dy[i];
-
-				if (checkRange(nx, ny) 
-						&& checkPopulation(now, new Node(nx, ny)) 
-						&& !v[nx][ny]) {
-
+				
+				if(checkRange(nx, ny) 
+						&& checkDiff(now, new Node(nx, ny)) 
+						&& !visited[nx][ny]) {
+					
 					queue.offer(new Node(nx, ny));
-					v[nx][ny] = true;
-					sum += map[nx][ny];
-					cnt++;
+					visited[nx][ny] = true;
 				}
 			}
 		}
-
-		// calculate average
-		int avg = sum / cnt;
-		average.add(avg);
+		
+		average.add(sum/cnt);
 	}
-
-	public static boolean checkPopulation(Node now, Node next) {
-		int p = map[now.x][now.y];
-		int q = map[next.x][next.y];
-		int difference = Math.abs(p - q);
-
-		return difference >= l && difference <= r;
+	
+	public static boolean checkDiff(Node cur, Node next) {
+		int m1 = map[cur.x][cur.y];
+		int m2 = map[next.x][next.y];
+		
+		int diff = Math.abs(m1-m2);
+		return diff >= l && diff <= r;
 	}
-
+	
+	public static boolean checkPopulation(Node cur) {
+		int m1 = map[cur.x][cur.y];
+		int m2;
+		
+		for(int i=0 ; i<4 ; i++) {
+			int nx = cur.x + dx[i];
+			int ny = cur.y + dy[i];
+			
+			if(!checkRange(nx, ny)) continue;
+			
+			m2 = map[nx][ny];
+			
+			int difference = Math.abs(m1 - m2);
+			if(difference >= l && difference <= r) return true;
+		}
+		
+		return false;
+	}
+	
 	public static boolean checkRange(int x, int y) {
 		return x >= 0 && x < n && y >= 0 && y < n;
 	}
@@ -140,7 +148,7 @@ public class boj_16234 {
 	public static class Node {
 		int x;
 		int y;
-
+		
 		public Node(int x, int y) {
 			this.x = x;
 			this.y = y;
