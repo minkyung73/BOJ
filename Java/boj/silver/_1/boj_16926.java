b0022e7aa1f049891f062b3b8c79b6de77598e21
minkyung <minkyung7365@gmail.com>
2024-02-08 00:25:54 +0900

    boj_16926

---
+++ Java/boj/silver/_1/boj_16926.java
@@ -0,0 +1,37 @@
+package boj.silver._1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_16926 {
+    // 배열 돌리기 1
+    static int n, m, r;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException{
+        init();
+
+
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        for(int i=0 ; i<n ; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0 ; j<m ; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+
+}
