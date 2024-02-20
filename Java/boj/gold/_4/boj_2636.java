fda4aff0c04c39b4a9530be4742523510a43ed81
minkyung <minkyung7365@gmail.com>
2024-02-20 13:36:28 +0900

    [RE]boj_2636 치즈

--- Java/boj/gold/_4/boj_2636.java
+++ Java/boj/gold/_4/boj_2636.java
@@ -1,5 +1,6 @@
 package boj.gold._4;
 
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,93 +9,76 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj_2636 {
-    // 치즈
+	// 치즈
+	static int n, m;
+	static int[][] map;
+	static int totalCheese, nowCheese;
+	static int hour;
+	
+	public static void main(String[] args) throws IOException{
+		init();
+		cheese();
+		System.out.println(hour + "\n" + nowCheese);
+	}
+	
+	public static void init() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		map = new int[n][m];
+		totalCheese = nowCheese = 0;
+		hour = 0;
+		
+		for(int i=0 ; i<n ; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0 ; j<m ; j++	) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 1) totalCheese++;
+			}
+		}
+	}
 
-    /**
-     * 초기 치즈 개수를 센다. -> cheese
-     * 치즈 개수가 0이 될 때까지 반복한다. -> cheeseCount
-     * BFS
-     *  배열의 값이 0일 경우 queue.offer
-     *  주변 탐색 시 1일 경우 cheese 개수를 줄인다.
-     */
-
-    static int n, m, cheese;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // initialize
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        visited = new boolean[n][m];
-        cheese = 0;
-
-        // map input
-        // count cheese
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 1) cheese++;
-            }
-        }
-
-        // calculate cheeseCount and hour
-        // function call - BFS
-        int cheeseCount = 0, hour = 0;
-        while (cheese != 0) {
-            cheeseCount = cheese;
-            visited = new boolean[n][m];
-            hour++;
-            BFS();
-        }
-
-        // output
-        System.out.println(hour + "\n" + cheeseCount);
-    }
-
-    public static void BFS() {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(0, 0));
-        visited[0][0] = true;
-
-        while (!queue.isEmpty()) {
-            Node now = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-
-                if (checkRange(nx, ny) && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-
-                    if (map[nx][ny] == 0) {
-                        queue.offer(new Node(nx, ny));
-                    } else {
-                        cheese--;
-                        map[nx][ny] = 0;
-                    }
-                }
-            }
-        }
-    }
-
-    public static boolean checkRange(int x, int y) {
-        return x >= 0 && x < n && y >= 0 && y < m;
-    }
-
-    public static class Node {
-        int x, y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	public static void cheese() {
+		while(totalCheese != 0) {
+			nowCheese = totalCheese;
+			BFS();
+			hour++;
+		}
+	}
+	
+	public static void BFS() {
+		Queue<Point> queue = new LinkedList<>();
+		boolean[][] visited = new boolean[n][m];
+		
+		int[] dx = {-1, 1, 0, 0};
+		int[] dy = {0, 0, -1, 1};
+		
+		queue.offer(new Point(0, 0));
+		visited[0][0] = true;
+		
+		while(!queue.isEmpty()) {
+			Point now = queue.poll();
+			
+			for(int i=0 ; i<4 ; i++) {
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
+				
+				if(checkRange(nx, ny) && !visited[nx][ny]) {
+					if(map[nx][ny] == 0) queue.offer(new Point(nx, ny));
+					else {
+						totalCheese--;
+						map[nx][ny] = 0;
+					}
+					visited[nx][ny] = true;
+				}
+			}
+		}
+	}
+	
+	public static boolean checkRange(int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < m;
+	}
 }
