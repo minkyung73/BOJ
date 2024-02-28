5c064b904ed7d1dcb1f69be3eda0e5464e2d151e
minkyung <minkyung7365@gmail.com>
2024-02-28 15:04:08 +0900

    [RE]boj_2178 미로탐색

--- Java/boj/silver/_1/boj_2178.java
+++ Java/boj/silver/_1/boj_2178.java
@@ -3,6 +3,7 @@ package boj.silver._1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -12,7 +13,8 @@ public class boj_2178 {
 
     static int n, m;
     static int[][] map;
-    static boolean[][] visited;
+    static boolean[][] Bvisited;
+    static int[][] Dvisited;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
     static int result = Integer.MAX_VALUE;
@@ -23,6 +25,7 @@ public class boj_2178 {
         BFS(0, 0);
         System.out.println(map[n-1][m-1]);
         
+//        Dvisited[0][0] = 1;
 //        DFS(0, 0, 1);
 //        System.out.println(result);
     }
@@ -35,7 +38,9 @@ public class boj_2178 {
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
-        visited = new boolean[n][m];
+        Bvisited = new boolean[n][m];
+        Dvisited = new int[n][m];
+        for(int i=0 ; i<n ; i++) Arrays.fill(Dvisited[i], Integer.MAX_VALUE);
 
         for(int i=0 ; i<n ; i++) {
             String str = br.readLine();
@@ -48,7 +53,7 @@ public class boj_2178 {
     public static void BFS(int x, int y) {
         Queue<Node> queue = new LinkedList<>();
         queue.add(new Node(x, y));
-        visited[0][0] = true;
+        Bvisited[0][0] = true;
 
         while (!queue.isEmpty()) {
             Node now = queue.poll();
@@ -57,15 +62,16 @@ public class boj_2178 {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
 
-                if(checkRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == 1) {
+                if(checkRange(nx, ny) && !Bvisited[nx][ny] && map[nx][ny] == 1) {
                     queue.add(new Node(nx, ny));
                     map[nx][ny] = map[now.x][now.y] + 1;
-                    visited[nx][ny] = true;
+                    Bvisited[nx][ny] = true;
                 }
             }
         }
     }
 
+    
     public static void DFS(int x, int y, int cnt) {
     	// basis part
     	if(x == n-1 && y == m-1) {
@@ -78,8 +84,8 @@ public class boj_2178 {
     		int nx = x + dx[i];
     		int ny = y + dy[i];
     		
-    		if(checkRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == 1) {
-    			visited[nx][ny] = true;
+    		if(checkRange(nx, ny) && Dvisited[nx][ny] > cnt+1 && map[nx][ny] > 0) {	
+    			Dvisited[nx][ny] = cnt+1;
     			DFS(nx, ny, cnt+1);
     		}
     	}
