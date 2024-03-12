eeac460488a5019a669d6d8281aad42af0a7f9e3
minkyung <minkyung7365@gmail.com>
2024-03-12 17:40:34 +0900

    [ADD]pgrms_최고의집합

---
+++ Java/programmers/Lv3/최고의집합.java
@@ -0,0 +1,22 @@
+package programmers.Lv3;
+import java.util.*;
+
+public class 최고의집합 {
+	class Solution {
+	    public int[] solution(int n, int s) {
+	        int[] answer = new int[n];
+	        
+	        if(n > s) return new int[]{-1};
+	        
+	        int num = s / n;
+	        for(int i=0 ; i<n ; i++) answer[i] = num;
+	        if(s % n != 0) {
+	            for(int i = n-1 ; i> n-1 - (s % n) ; i--) {
+	                answer[i]++;
+	            }
+	        }
+	        
+	        return answer;
+	    }
+	}
+}
